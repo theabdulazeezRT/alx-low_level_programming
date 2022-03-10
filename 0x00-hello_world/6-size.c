fad97462717b824520f071c32143f77b5c31b864
Abdulazeez Ridwan Temitope <abdulazeezridwantemitop001@gmail.com>
2022-03-10 12:47:52 -0800

    13

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a;
+int b;
+long c;
+long long d;
+float e;
+
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of a int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long: %lu byte(s)\n", sizeof(d));
+printf("Size of a float: %lu byte(s)\n", sizeof(e));
+
+return (0);
+} 
